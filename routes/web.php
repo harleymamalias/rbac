bce7bcd3b0ff303a8622b319a775736661a2c41f
Harley Mamalias <harleymamalias@gmail.com>
2024-07-01 00:05:43 +0800

    added route for assigning permissions and roles

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::middleware('auth')->group(function(){
     Route::get('/admin', [AdminController::class, 'index'])->name('dash')->middleware('role:admin');
     Route::get('/admin/users',[AdminController::class,'manageUsers'])->name('usertool')->middleware('role:admin');
 
+    Route::put('/assign-permissions/{user}', [AdminController::class, 'assignPermissions'])->name('assign.permissions');
+    Route::put('/assign-roles/{user}', [AdminController::class, 'assignRoles'])->name('assign.roles');
+
+
     Route::get('/acctg/new',[BookController::class,'newLedgerEntry'])->middleware(['role:admin,bookeeper,auditor'])->name('newledger');
     Route::post('/acctg/new',[BookController::class,'saveNewLedgerEntry'])->middleware('role:admin,bookeeper')->name('saveledger');
     Route::get('/acctg/view/all',[BookController::class,'showAllLedgers'])->middleware(['role:admin,bookeeper,auditor,audasst'])->name('ledgers');
